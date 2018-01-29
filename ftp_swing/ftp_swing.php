6067bd3bbc757f4ba0f4204f23fe8f74c72e17f3
Tuhin <tuhin.sheikh@progoti.net>
2018-01-29 22:39:15 +0000

    added ftp_swing plugin

---
+++ ftp_swing/ftp_swing.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: FTP Swing
+Plugin URI: https://github.com/
+Description: Sample plugin to allow user upload file to a third party FTP server. https://stackoverflow.com/questions/48463827/allow-customers-to-upload-files-directly-to-local-ftp-server-on-wordpress-websit/48464675#48464675
+Version: 0.1
+Author: Tuhin Sheikh
+*/
+
+// hook for adding admin menus
+add_action( 'admin_menu', 'ts_add_pages' );
+
+// function for the hook above
+function ts_add_pages()
+{
+
+}
