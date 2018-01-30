a5e89a491fce5ed0816a63f4261c0b7cc708ac81
Tuhin Sheikh <tuhin.sheikh@gmail.com>
2018-01-30 17:30:32 +0000

    adding admin site page
    adding admin site page which will be available under Tools menu.

--- ftp_swing/ftp_swing.php
+++ ftp_swing/ftp_swing.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: FTP Swing
+Plugin Name: FTP Upload
 Plugin URI: https://github.com/
 Description: Sample plugin to allow user upload file to a third party FTP server. https://stackoverflow.com/questions/48463827/allow-customers-to-upload-files-directly-to-local-ftp-server-on-wordpress-websit/48464675#48464675
 Version: 0.1
@@ -13,5 +13,10 @@ add_action( 'admin_menu', 'ts_add_pages' );
 // function for the hook above
 function ts_add_pages()
 {
+  add_submenu_page('tools.php', 'FTP Upload', 'FTP Upload', 'activate_plugins', 'what_is_here','display_ftp_upload_page');
+}
 
+function display_ftp_upload_page()
+{
+  include_once('html/ftp_upload_page.php');
 }
